339c5f3491eae88eb50bd01cdea8d5cf73f3269f
BraydenV42 <brayriley44@gmail.com>
2025-10-02 12:49:25 -0400

    Update TempConverter.java

--- src/main/java/TempConverter.java
+++ src/main/java/TempConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TempConverter {
     public static void main(String[] args) {
         // TODO: Implement temperature converter
@@ -7,5 +9,36 @@ public class TempConverter {
         // - Use formulas: C = (F - 32) * 5/9 and F = C * 9/5 + 32
         // - Display conversion results
         // - Handle invalid menu choices
+        boolean running = true;
+        double Celsius;
+        double Fahrenheit;
+        Scanner sc = new Scanner(System.in);
+
+        while (running) {
+            int choice;
+            System.out.println("------------Temperature Converter-------------");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("3. Exit");
+            System.out.println("--------------------------------------");
+            System.out.println("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    Celsius = sc.nextDouble();
+                    Fahrenheit = Celsius * 9/5 + 32;
+                    System.out.println(Fahrenheit);
+                    break;
+                case 2:
+                    Fahrenheit = sc.nextDouble();
+                    Celsius = (Fahrenheit - 32) * 5/9;
+                    System.out.println(Celsius);
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    running = false;
+                    break;
+            }
+        }
     }
 }
